afe22e8a73f0b09a48df166e852fdfe56bb16700
Vladimir Trifonov <vladimir.trifonov@limechain.tech>
2024-08-05 15:14:45 +0300

    fix: remove unused tracing

--- examples/preconf/src/main.rs
+++ examples/preconf/src/main.rs
@@ -1,17 +1,6 @@
 use commit_boost::prelude::*;
 use eyre::Result;
-use lazy_static::lazy_static;
-use prometheus::{IntCounter, Registry};
 use tracing::{error, info};
-
-// You can define custom metrics and a custom registry for the business logic of
-// your module. These will be automatically scaped by the Prometheus server
-lazy_static! {
-    pub static ref MY_CUSTOM_REGISTRY: prometheus::Registry =
-        Registry::new_custom(Some("preconf".to_string()), None).unwrap();
-    pub static ref SIG_RECEIVED_COUNTER: IntCounter =
-        IntCounter::new("signature_received", "successful signatures requests received").unwrap();
-}
 
 struct PreconfService {
     config: StartCommitModuleConfig<()>,
@@ -27,11 +16,6 @@ impl PreconfService {
 async fn main() -> Result<()> {
     color_eyre::install()?;
     initialize_tracing_log();
-
-    // Remember to register all your metrics before starting the process
-    MY_CUSTOM_REGISTRY.register(Box::new(SIG_RECEIVED_COUNTER.clone()))?;
-    // Spin up a server that exposes the /metrics endpoint to Prometheus
-    MetricsProvider::load_and_run(MY_CUSTOM_REGISTRY.clone())?;
 
     match load_commit_module_config::<()>() {
         Ok(config) => {
